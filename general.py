2e8b3669b1bbf5e18bc6a3e5059a5f56b1f27b6b
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2016-06-26 09:27:54 +1000

    Created Project

--- general.py
+++ general.py
@@ -1,22 +1,36 @@
 import os
 
+
 # Each website you crawl is a separate project
 def create_project_dir(directory):
     if not os.path.exists(directory):
         print("Creating project " + directory)
         os.makedirs(directory)
 
+
 # Create queue and crawled files (if not created)
 def create_data_files(project_name, base_url):
-    queue = project_name + "queue.txt"
-    crawled = project_name + "crawled.txt"
+    queue = project_name + "/queue.txt"
+    crawled = project_name + "/crawled.txt"
     if not os.path.isfile(queue):
         write_file(queue, base_url)
     if not os.path.isfile(crawled):
         write_file(crawled, "")
+
 
 # Create a new file
 def write_file(path, data):
     f = open(path, "w")
     f.write(data)
 
+
+# Add data onto an existing file
+def append_to_file(path, data):
+    with open(path, "a") as file:
+        file.write(data + "\n")
+
+
+# Delete the contents of a file
+def delete_file_contents(path):
+    with open(path, "w"):
+        pass
